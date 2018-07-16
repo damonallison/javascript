b4b238dbda24d7ebd4e6773d69770d52a00682b2
Damon Allison <damon.allison@chrobinson.com>
2018-07-16 07:54:35 -0500

    Finishing up YDKJS Book 4 - Async & Performance, Chapter 3 - Promises

--- src/__tests__/5-async/promises.test.js
+++ src/__tests__/5-async/promises.test.js
@@ -19,9 +19,7 @@ const setsEqual = require('sets-equal');
 
 test("simple-promise", () => {
 
-    let x = 10; // Update this value to trigger rejection.
-
-    let results = new Set();
+    let chainResults = new Set();
     let listenerOrder = [];
 
     //
@@ -30,10 +28,9 @@ test("simple-promise", () => {
     // any listener will be notified when subscribing
     //
     var promise = new Promise((resolve, reject) => {
-
         // do something async.
         setTimeout(() => {
-            x == 10 ? resolve("success") : reject(new Error("failed"));
+            resolve("success");
         }, 50 /* ms */);
     });
 
@@ -41,39 +38,34 @@ test("simple-promise", () => {
     // Note the promise chain (then / catch) is *always invoked*.
     //
     promise.then(success => {
-        listenerOrder.push(1);
         expect(success).toBe("success");
-        results.add(success);
-        return "successful";
-    }).catch(err => {
-        expect(err instanceof Error).toBeTruthy();
-        expect(err.message).toBe("failed");
-        return "failure";
+
+        //
+        // Since this is the first listener attached, it will be the first one invoked.
+        //
+        listenerOrder.push(1);
+        chainResults.add(success);
+        return "first";
     }).then(val => {
         //
         // this chained promise will be invoked in both
         // success and failure cases
         //
-        expect(val).toBe("successful");
-        results.add(val);
-        return "final";
+        expect(val).toBe("first");
+        chainResults.add(val);
+        return "second";
     }).then(val => {
-        expect(val).toBe("final");
-        expect(setsEqual(new Set(["success", "successful"]), results)).toBeTruthy();
+        expect(val).toBe("second");
+        expect(setsEqual(new Set(["success", "first"]), chainResults)).toBeTruthy();
     });
 
     //
     // A promise can have multiple listeners.
     //
-    promise.then(
-        (val) => {
-            listenerOrder.push(2);
-            expect(val).toBe("success");
-        },
-        (err) => {
-            throw err;
-        }
-    );
+    promise.then(val => {
+        listenerOrder.push(2);
+        expect(val).toBe("success");
+    });
 
     //
     // Important Note:
@@ -101,6 +93,10 @@ test("settled-promise", () => {
     // Create a promise that is already resolved.
     let p = Promise.resolve(100);
 
+    // Even tho the promise is already resolved, any handler attached to
+    // the promise will be invoked. This is a great feature, it ensures
+    // we do not encounter race conditions when the promise is fulfilled
+    // before the handler is attached.
     let called = false;
     p.then(value => {
         called = true;
@@ -110,27 +106,62 @@ test("settled-promise", () => {
     return p;
 });
 
+//
+// Promise.all() is a "gate" to wait on 1->n parallel / concurrent tasks to
+// finish before continuing.
+//
 test("multiple-promises", () => {
     //
     // Promise.all takes an iterable of promises and itself returns
     // a promise that is resolved after all it's promises are resolved.
     //
     let p1 = new Promise((resolve, reject) => {
-        setTimeout(() => resolve(1), 100);
+        setTimeout(() => resolve(1), 50);
     });
     let p2 = new Promise((resolve, reject) => {
-        setTimeout(() => resolve(2), 200);
+        setTimeout(() => resolve(2), 10);
     });
 
     return new Promise((resolve, reject) => {
+        //
+        // Results will be returned in the order they are given to Promise.all
+        //
         let p3 = Promise.all([p1, p2]);
         p3.then(value => {
-            resolve();
-            expect(value).toEqual([1, 2]);
+            expect(arraysEqual([1, 2], value)).toBeTruthy();
+            resolve("done");
         }).catch(error => {
             reject(error);
         });
     });
+});
+
+//
+// What happens if some promises given to Promise.all() succeed and others fail?
+//
+// Both promises execute, however *only* the error handler will fire.
+//
+test("multiple-promises-resolve-and-reject", () => {
+
+    let executedPromises = new Set();
+    let p1 = new Promise((resolve, reject) => {
+        executedPromises.add("p1");
+        resolve("good");
+    });
+    let p2 = new Promise((resolve, reject) => {
+        executedPromises.add("p2");
+        reject("bad");
+    });
+
+    // Always attach a `catch` to every promise, including the results from Promise.all()
+    return Promise.all([p1, p2])
+        .then(val => {
+            // will *not* fire, even tho `p1` resolved successfully.
+            expect(false).toBeTruthy();
+        }).catch(err => {
+            expect(err).toBe("bad");
+            expect(setsEqual(new Set(["p1", "p2"]), executedPromises)).toBeTruthy();
+        });
 });
 
 //
@@ -178,11 +209,60 @@ test("nested-promise", () => {
     });
 });
 
+//
+// Error Handling
+//
+
+//
+// `then` takes two callbacks.
+//
+// 1. onFulfilled(val). This will be invoked when the promise normally
+//    `resolve`s.
+// 2. onRejected(err). This will be invoked when the promise is `reject`ed.
+//
+// Both functions are optional. If you omit the first (by using `null`), an
+// implicit handler will be created which simply returns the value.
+//
+//
+// ------------------------------------------------------------------------
+// This:
+//
+// p.then(null, (err) => console.log(err));
+//
+// Will expand into:
+//
+// p.then((val) => { return val; }, (err) => { console.log(err); }
+// );
+//
+// ------------------------------------------------------------------------
+// This:
+//
+// p.then(val => { console.log(val); });
+//
+// Will expand into:
+//
+// p.then(
+//   (val) => { console.log(val); },
+//   (err) => { throw err; }
+// );
+//
+// Always include an exception handler, even if just for logging, at the end of
+// your promise chain.
+//
+test("error-handling", () => {
+
+    const p = new Promise((resolve, reject) => {
+        reject(new Error("oops"));
+    });
+    p.catch(err => {
+        expect(err.message).toBe("oops");
+    })
+});
 
 //
 // Errors that occur in a `then` handler, will be handled to the `catch`.
 //
-test("promise-errors-call-reject", () => {
+test("error-handling-within-then", () => {
 
     let p = new Promise((resolve, reject) => {
         resolve("done");
