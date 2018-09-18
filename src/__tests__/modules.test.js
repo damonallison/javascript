8b7f4062137a2d764467e2de03108bce403e556e
Damon Allison <damon.allison@chrobinson.com>
2018-09-18 09:15:23 -0500

    Starting with Modules...

--- src/__tests__/modules.test.js
+++ src/__tests__/modules.test.js
@@ -3,9 +3,36 @@
 //
 // ES6 introduces "modules"
 //
-// Prior to modules, javascript was essentially a global, "shared everything"
-// environment. Modules allow us to encapsulate logic and selectively import
-// pieces of functionality into our current scope.
+// Modules allow you to abstract away functionality into individual files and
+// selectively import pieces of functionality into our current scope.
+//
+// Prior to ES6, modules were "faked" by creating functions which returned
+// objects that encapsulated functions and closed over state.
+//
+// The state of modules in JS is a holy mess. There wasn't an official standard
+// until ES6. By then, the community created their own module format, CommonJS,
+// which is used by node.
+//
+// These examples use CommonJS.
+
+const calculator = require("../modules/calculator");
+
+test("module-importing", () => {
+    expect(calculator.add(2, 2)).toBe(4);
+    expect(calculator.subtract(10, 2)).toBe(8);
+});
+
+// ----------------------------------------------------------------------------------
+//
+// NOTE:
+//
+// These notes are about ES6 modules. I wasn't able to get ES6 modules working.
+// I need to understand what execution environment `npm test` runs under and
+// what module system(s) it supports.
+//
+// ----------------------------------------------------------------------------------
+
+//
 //
 // Modules:
 //
@@ -25,14 +52,11 @@
 // All non-default elements being imported from a module must be enclosed in
 // curly braces.
 //
-import { echo } from "../modules/calculator";
-
+// Default elements being imported do *not* need to be enclosed in curly braces.
+// This is the most common way to implement members, since ideally each module
+// should only export a single, default, element.
 //
-// Default elements being imported do *not* need to be enclosed
-// in curly braces. This is the most common way to implement
-// members, since ideally each module should only export a single, default, element.
-//
-import Calculator from "../modules/calculator";
+// import Calculator from "../modules/calculator";
 
 //
 // A "namespace import"
@@ -40,15 +64,15 @@ import Calculator from "../modules/calculator";
 // All exports are referenced via the `CalculatorModule` object.
 // The `CalculatorObject` becomes the "namespace" in which you access the module's exports.
 //
-import * as CalculatorModule from "../modules/calculator";
+// import * as CalculatorModule from "../modules/calculator";
 
-test("importing", () => {
+// test("importing", () => {
 
-    expect(Calculator.add(2, 2)).toEqual(4);
-    expect(echo("test")).toEqual("test");
+//     expect(Calculator.add(2, 2)).toEqual(4);
+//     expect(echo("test")).toEqual("test");
 
-    // Using namespace imports, access the default member with `default`.
-    expect(CalculatorModule.default.add(2, 2)).toEqual(4);
-    expect(CalculatorModule.echo("test")).toEqual("test");
+//     // Using namespace imports, access the default member with `default`.
+//     expect(CalculatorModule.default.add(2, 2)).toEqual(4);
+//     expect(CalculatorModule.echo("test")).toEqual("test");
 
-});
+// });
--- src/modules/calculator.js
+++ src/modules/calculator.js
@@ -1,44 +1,19 @@
 //
-// Module example.
+// CommonJS (Node.js) module example.
 //
-// Exporting:
+// * Each js file is a module.
+// * Objects / functions / properties are added to the module by adding them to
+//   the special `exports` object.
 //
-// There are two ways to export code from modules.
-//
-// 1. Place `export` in front of any variable or function to export it.
-// export function sum()
-//
-// 2. Export it later
-// function sub(x, y) {
-//   return x - y;
-//}
-//
-// export { sub }
-//
-//
-// Default export
-//
-// Each module can have a single default export.
-//
-// This simplifies the import statement - avoiding the use of
-//
-// import Calculator from "../modules/calculator";
-//
+function add(x, y) {
+    return x + y;
+};
 
-export default class Calculator {
+function subtract(x, y) {
+    return x - y;
+};
 
-    static add(x, y) {
-        return x + y;
-    }
-
-    static subtract(x, y) {
-        return x - y;
-    }
-
-}
-
-function echo(s) {
-    return s;
-}
-
-export { echo };
+module.exports = {
+    add,
+    subtract
+};
